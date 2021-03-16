5b843afbd61f6a082c0a13fd346c7fdb5822f405
Gareth Hutchins <Gareth.Hutchins@gmail.com>
2021-03-16 23:02:49 +0000

    Added Training page and pass documents to tika and get results back

--- userexperience/urls.py
+++ userexperience/urls.py
@@ -4,6 +4,9 @@ from userexperience import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("userexperience/<name>",views.hello_there,name="hello_there"),
+    path("train/", views.train, name="train"),
+    path("test/", views.test, name="test"),
+    path("settings/", views.settings, name="settings"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
 ]
--- userexperience/views.py
+++ userexperience/views.py
@@ -1,7 +1,11 @@
 import re
+import requests
+import mimetypes
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils.timezone import datetime
+from django.core.files.storage import FileSystemStorage
+
 # Create your views here.
 def home(request):
     return render(request, "userexperience/home.html")
@@ -13,6 +17,40 @@ def hello_there(request, name):
            'date':datetime.now()
        }
    )
+def test(request):
+    return render(request, "userexperience/test.html")
+def train(request):
+    #Check to see if a file has been selected
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        #Save the file so it can be read
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        full_file_path = fs.location + "/" + filename
+        #Set the tika request url
+        url = "http://localhost:9998/tika"
+        #open the file for reading
+        with open(full_file_path, 'rb') as f:
+            payload=f.read()
+        #Guess the Mime Type and set the content type for the tika request
+        mime = mimetypes.guess_type(full_file_path)
+        headers = {
+                'Content-Type': mime[0]
+                }
+        response = requests.request("PUT", url, headers=headers, data=payload)
+        #Now delete the file
+        fs.delete(filename)
+        #Now send the text results back to the testing page
+        args = {'result' : response.text }
+        return render(request, 'userexperience/train.html', args)
+    return render(request, "userexperience/train.html")
+def settings(request):
+    #Save the settings
+    print(getattr(app_settings, 'TIKA', 'default value'))
+    if request.method == 'POST':
+        #settings.TIKA = 'Test'
+        print(getattr(app_settings, 'TIKA', 'default value'))
+    return render(request, "userexperience/settings.html")
 def about(request):
     return render(request, "userexperience/about.html")
 def contact(request):
